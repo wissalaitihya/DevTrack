6329ad250b5035e1385ba2824415611dad3b63d7
wissalaitihya <wissalaitihya@gmail.com>
2026-05-08 09:26:02 +0100

    fix errors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,11 +13,10 @@
         <a href="{{ route('projects.index') }}" class="navbar-brand">
             🚀 DevTrack
         </a>
-//
         <ul class="navbar-nav">
             @auth
                 <li><a href="{{ route('projects.index') }}">Dashboard</a></li>
-                <li><a href="{{ route('projects.archived') }}">Archives</a></li>
+                <li><a href="{{ route('projects.archives') }}">Archives</a></li>
                 <li>
                     <form action="{{ route('logout') }}" method="POST">
                         @csrf
